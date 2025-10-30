a41a283ed0faa05ff1a52a440e8f9df6cb4ce7d0
Nikita Tale <talenikita3@gmail.com>
2025-10-30 19:05:20 +0530

    Delete backend directory

--- backend/models/cartItem.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const cartItemSchema = new mongoose.Schema({
-  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-  qty: Number
-});
-
-export default mongoose.model("CartItem", cartItemSchema);
